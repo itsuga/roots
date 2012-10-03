dbd0ebd0a1158bfa882b6b450cf2fb09502ebef8
Agusti Belloc <agusti@bibtic.cat>
2012-10-03 03:53:22 +0200

    Acat : modified .navbar-top and #wpadminbar css

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,7 +1,7 @@
-<header id="banner" class="navbar navbar-fixed-top" role="banner">
+<header id="banner" class="navbar navbar-fixed-top navbar-inverse" role="banner">
   <div class="navbar-inner">
     <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <a class="btn btn-navbar btn-inverse" data-toggle="collapse" data-target=".nav-collapse">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
