4f59149394c2facdb12c87c9080aed00dfc5644d
Agusti Belloc <agusti@bibtic.cat>
2012-10-03 02:40:00 +0200

    fixed bug : upstream changed fuction roots_entry_meta() to get_template_part('templates/entry-meta')

--- acat/format.php
+++ acat/format.php
@@ -5,7 +5,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('span6'); ?>>
         <header>
           <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-          <?php roots_entry_meta(); ?>
+          <?php get_template_part('templates/entry-meta'); ?>
         </header>
         <div class="entry-content">
           <?php 
