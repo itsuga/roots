71c6ae336cdb20b4cd01d8f8fd8e3ee2689b2a2e
itsuga <itsuga@agusti.cat>
2013-02-15 20:04:14 +0100

    filter to remove wordpress logo from admin bar

--- functions.php
+++ functions.php
@@ -19,3 +19,13 @@ require_once locate_template('/lib/custom.php');          // Custom functions
 
 // Register Custom Gallery
 require_once('wp_bootstrap_gallery.php');
+
+// filter to remove wordpress logo/pages from admin bar
+function annointed_admin_bar_remove() {
+        global $wp_admin_bar;
+
+        /* Remove their stuff */
+        $wp_admin_bar->remove_menu('wp-logo');
+}
+
+add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
