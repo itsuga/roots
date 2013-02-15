891f7d99b055c54692665d010490a1d9d7f7bd88
itsuga <itsuga@agusti.cat>
2013-02-15 01:19:56 +0100

    Register Custom Gallery in functions.php

--- functions.php
+++ functions.php
@@ -16,3 +16,6 @@ require_once locate_template('/lib/htaccess.php');        // Rewrites for assets
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
+
+// Register Custom Gallery
+require_once('wp_bootstrap_gallery.php');
